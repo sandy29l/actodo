d479e9ca78aa8d0cb91c056c3156b5db3a44cfba
sandy29l <sandyborn2904@gmail.com>
2026-05-13 11:30:21 +0530

    updated recently

--- src/index.js
+++ src/index.js
@@ -2,21 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Rdm from './rdm.js';
 // import Form from './formcomp.js';
-import Login from'./actodo/login.jsx';
-import Actodo from './actodo/actodo.js'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Signup from './actodo/signup.jsx'; 
 import App from './actodo/app.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-  {/* <Actodo></Actodo> */}
-  {/* <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Login/>}></Route>
-    <Route path='/signup' element={<Signup/>}></Route>
-  </Routes>
-  </BrowserRouter> */}
   <App></App>
   
   </div>
